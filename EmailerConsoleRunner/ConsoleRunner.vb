a6c149afc60a2e302dbf325fa98f56325e3267bc
Ramezcua <ramezcua7@gmail.com>
2015-08-19 11:43:28 -0500

    Add Console project to debug Emailer

---
+++ EmailerConsoleRunner/ConsoleRunner.vb
@@ -0,0 +1,13 @@
+﻿Imports NLog
+Imports Emailer.EmailerManager
+
+Module ConsoleRunner
+
+    Sub Main()
+        Console.WriteLine("Running Email Managing")
+        Emailer.EmailerManager.Run()
+        Console.WriteLine("Finished Running Email Mananaging")
+        Console.ReadKey()
+    End Sub
+
+End Module
---
+++ EmailerConsoleRunner/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("EmailerConsoleRunner")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("EmailerConsoleRunner")> 
+<Assembly: AssemblyCopyright("Copyright ©  2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("227da5db-d541-4f8a-9026-ad8a4a3d86b8")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
